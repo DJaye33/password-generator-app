1ff170951d1438d2e7dd15ecd9c655aac8801554
Danny JeanLouis <dlouis3386@gmail.com>
2022-08-31 15:26:00 -0400

    add slider track script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+var slider = document.querySelector("#myRange");
+var changeSliderBgColor = function () {
+    var sliderMultiplier = 5;
+    var value = Number(slider.value) * sliderMultiplier;
+    var backgroundColor = "linear-gradient(90deg, var(--color-neon-green) ".concat(value, "%, var(--color-very-dark-gray) ").concat(value, "%)");
+    slider.style.background = backgroundColor;
+};
+slider.addEventListener("input", changeSliderBgColor);
--- scripts.ts
+++ scripts.ts
@@ -1,1 +1,10 @@
+const slider: HTMLInputElement = document.querySelector("#myRange")!;
 
+const changeSliderBgColor = (): void => {
+  const sliderMultiplier = 5;
+  const value = Number(slider.value) * sliderMultiplier;
+  const backgroundColor = `linear-gradient(90deg, var(--color-neon-green) ${value}%, var(--color-very-dark-gray) ${value}%)`;
+  slider.style.background = backgroundColor;
+};
+
+slider.addEventListener("input", changeSliderBgColor);
